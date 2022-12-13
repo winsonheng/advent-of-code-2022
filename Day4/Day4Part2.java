b41441b8174cd391e834a33c469344d46f806e52
winson heng <winsonh5147@gmail.com>
2022-12-13 10:20:34 +0800

    Completed Day 4

---
+++ Day4/Day4Part2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.stream.Stream;
+
+public class Day4Part2 {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String fileName = "4.in";
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        int count = 0;
+        
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] split = line.split(",");
+            Integer[] first = Stream.of(split[0].split("-")).map(Integer::valueOf).toArray(Integer[]::new);
+            Integer[] second = Stream.of(split[1].split("-")).map(Integer::valueOf).toArray(Integer[]::new);
+            if (first[1] >= second[0] && first[0] <= second[1]) {
+                ++count;
+            }
+        }
+        br.close();
+
+        System.out.println(count);
+    }
+}
